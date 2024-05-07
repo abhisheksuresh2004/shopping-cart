478085dcccc0a2fc208e221dccefe282b0cfcb55
Abhishek Suresh <abhisheksuresh2004@gmail.com>
2024-05-06 20:58:47 -0700

    syntax

--- app/cart/page.js
+++ app/cart/page.js
@@ -4,23 +4,21 @@ import CartProduct from "@/components/CartProduct";
 import React from "react";
 
 export default function CartPage() {
-  const { cart } = useCart();
-  console.log(cart);
-  const totalPrice = cart.reduce((num, item) => num + item.price, 0);
+  const { check } = useCart();
+  console.log(check);
   return (
     <section>
       <h2 className="section-title">Items that you have added to cart can be seen here</h2>
+
       <div className="cart-page">
+
         <div className="cart-products">
-          {cart?.map((im) => {
+
+          {check?.map((im) => {
             return <CartProduct data={im} key={im.id} />;
           })}
         </div>
-        {/* <div className="total-card">
-          <h1>Cart Total</h1>
-          <hr />
-          <h2>${totalPrice}</h2>
-        </div> */}
+       
       </div>
     </section>
   );
